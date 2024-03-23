b9f7f7b5a7e6c26885ee9913ccc3670d53eec2fa
Rafael Nakata <rafaelk.nakata@gmail.com>
2024-03-23 20:35:10 -0300

    rename defect and labeled

--- rename/rename.py
+++ rename/rename.py
@@ -1,5 +1,16 @@
 import subprocess
 
+defect_labels = []
+
+def get_labels(meta_filename: str):
+    with open(meta_filename) as f:
+        meta = json.loads(f.read())
+    return list(map(lambda shape: shape['label'], meta['shapes']))
+
+def insert_prefix_to_filename(prefix, filename):
+    subprocess.run(['mv', filename, './' + prefix + filename[2:]])
+
+
 output_images = subprocess.run(['find', '-name', '*.jpg'], stdout=subprocess.PIPE)
 filenames_images = output_images.stdout.decode('utf-8').split()
 
@@ -8,9 +19,12 @@ filenames_meta = output_meta.stdout.decode('utf-8').split()
 
 for filename in filenames_images:
     if filename.replace('jpg', 'json') in filenames_meta:
-        
-        subprocess.run(['mv', filename, './' + 'labeled_' + filename[2:]])
+        labels = get_labels(filename.replace('jpg', 'json'))
+        if set(defect_labels).intersection(labels):
+            insert_prefix_to_filename('defect_' + '_'.join(sorted(labels)), filename)
+        else:
+            insert_prefix_to_filename('labeled_' + '_'.join(sorted(labels)), filename)
     else:
-        subprocess.run(['mv', filename, './' + 'unlabeled_' + filename[2:]])
+        insert_prefix_to_filename('unlabeled_', filename)
 
 
